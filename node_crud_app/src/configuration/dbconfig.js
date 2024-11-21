9959e70d9fc8d11cf9bae6e0e159185729d71b25
Kalil20000task <166611304+Kalil20000task@users.noreply.github.com>
2024-11-22 02:23:59 +0300

    Update dbconfig.js

--- node_crud_app/src/configuration/dbconfig.js
+++ node_crud_app/src/configuration/dbconfig.js
@@ -1,16 +1,26 @@
-const mongoose= require('mongoose');
-mongoose.connect("mongodb://localhost:27017/crud",{
+const mongoose = require('mongoose');
+
+const uri ="mongodb+srv://teamekaleab925:kalilmongopassword@trainupcluster.1y7tu.mongodb.net/";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
+// mongoose.connection.on("connected",() =>{
+// const mongoose= require('mongoose');
+// mongoose.connect("mongodb://localhost:27017/crud",{
     
-});
-mongoose.connection.on("connected",() =>{
-    console.log("connected to mongo db");
+// });
 
-});
-mongoose.connection.on("error",(err) =>{
-    console.error(`mongodb conecttion error:${err}`);
+//     console.log("connected to mongo db");
 
-});
+// });
+// mongoose.connection.on("error",(err) =>{
+//     console.error(`mongodb conecttion error:${err}`);
+
+// });
 module.exports=mongoose;
+
+
+
 
 
 
