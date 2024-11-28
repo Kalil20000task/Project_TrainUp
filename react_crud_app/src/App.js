e9f9afa9db21127d64b85fef34faf7ed5a808b05
Kalil20000task <166611304+Kalil20000task@users.noreply.github.com>
2024-11-28 11:00:17 +0300

    Update App.js

--- react_crud_app/src/App.js
+++ react_crud_app/src/App.js
@@ -33,7 +33,10 @@ function App() {
         <Route path="/*" element={<Dashboard />} />
         <Route path="/update" element={<Update />} />
         <Route path="/about_us" element={<AboutUs />} />
-        <Route path="/register_user" element={<RegistrationForm />} />
+    <Route
+          path="/register_user"
+          element={<Navigate to="/dashboard" replace /> : <RegistrationForm />}
+        />
         <Route path="/login" element={<Dashboard />} />
 
         {/* Admin Access Route */}
